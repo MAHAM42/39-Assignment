64810728bc2980824fe0394c7942cd20974d15ab
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 09:41:19 -0700

    39-Assignment
    TypeScript 45-Assignment of Q39 with Node.js

---
+++ main39.ts
@@ -0,0 +1,25 @@
+// ASSIGNMENT NO 39:
+
+/*
+City Names: Write a function called city_country() that takes in the name of a city and its country.
+ The function should return a string formatted like this:
+
+"Lahore, Pakistan"
+
+Call your function with at least three city-country pairs, and print the value that’s returned.
+*/
+
+console.log("ASSIGNMENT NO 39:");
+
+// Creating a function with parameters which return a values in string:
+
+function city_country(city: string, country: string) : string{
+    return `${city}, ${country}`;
+}
+
+// Calling the function and print the returned value:
+console.log (city_country("Karachi", "Pakistan"));
+
+console.log (city_country("Tokyo", "Japan"));
+
+console.log (city_country("Berlin", "Germany"));
